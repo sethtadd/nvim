c50759a6e7f56e25bdd82e8bcdda0c8f2ca47de3
sethtadd <sethtaddiken@gmail.com>
2024-12-15 11:26:50 -0800

    Configure Oil to auto refresh and to not permanently delete files

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,6 +2,9 @@ return {
   'stevearc/oil.nvim',
   ---@module 'oil'
   ---@type oil.SetupOpts
-  opts = {},
+  opts = {
+    delete_to_trash = true,   -- Don't permanently delete files from oil
+    watch_for_changes = true, -- Watch filesystem for changes and auto-reload oil
+  },
   dependencies = { "nvim-tree/nvim-web-devicons" },
 }
